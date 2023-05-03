43b0e8a32e7135bb3f55ca946bc5e72eb48f4765
Siddhant sharma <hitechsidu992@gmail.com>
2023-05-03 10:20:49 +0530

    Change url

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
-const url="mongodb://127.0.0.1:27017/Alston_Social_Media"
-// const url = 'mongodb+srv://Sidhu:Sidu&7879@cluster0.fca4n63.mongodb.net/Alston_Social_Media';
+// const url="mongodb://127.0.0.1:27017/Alston_Social_Media"
+const url = 'mongodb+srv://Sidhu:Sidu&7879@cluster0.fca4n63.mongodb.net/Alston_Social_Media';
 
 
 mongoose.connect(url);
